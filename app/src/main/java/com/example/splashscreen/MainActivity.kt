0e7aecd86ee162898f6dc0824b482108822f377e
Bahadır Rahmi Üngör <89974128+BahadirRahmiUngor@users.noreply.github.com>
2021-10-26 21:57:30 +0300

    Initial commit.

---
+++ app/src/main/java/com/example/splashscreen/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.splashscreen
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.hw2.util.inTransaction
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.test_activity_screen)
+
+
+        //call extension function in util/extensions
+        supportFragmentManager.inTransaction{
+            add(R.id.main_activity, TestFragment())
+        }
+    }
+}
